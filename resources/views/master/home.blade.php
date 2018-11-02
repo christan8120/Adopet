ff4450d95b4b6579018ef2bd9525b84b552df0a7
Jordy Andreas <jordyandreas76@gmail.com>
2018-11-02 21:33:41 +0700

    fix home.blade bagian tampilan pet

--- resources/views/master/home.blade.php
+++ resources/views/master/home.blade.php
@@ -46,31 +46,90 @@
             cursor: pointer;
         }
         .img-fluid{
-            width:250px;
-            height:250px;
+            width:30%;
+            height:170px;
             margin:1%;
         }
         
+        .img-fluid-modal{
+                width:300px;
+                height:300px;
+                margin:1%;
+            }
+
+
+        @media(max-width: 576px){
+            .img-fluid{
+                width:30%;
+                height:100px;
+                margin:1%;
+            }
+
+            .img-fluid-modal{
+                width:170px;
+                height:170px;
+                margin:1%;
+            }
+        }
 
 
     </style>
-
+    
     <div class="limiter">
         <div class="container-login100">
-            <div class="wrap-login100" style="text-align:center;">
+        <div class="add">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+        </div>
+            <div class="wrap-login100">
 
                 @if(count($pets) > 0)
-                    @foreach($pets as $pet)
                     <div class="col-lg-3 col-md-4 col-xs-6">
+                    @foreach($pets as $pet)
                         <img id="myImg{{$pet->id}}" class="img-fluid img-thumbnail" src="{{asset($pet->picture_src)}}" alt="" onclick="imgOnClick({{$pet->id}})">
+                    @endforeach
                     </div>
-
+                    
+                    @foreach($pets as $pet)
                     <div id="myModal{{$pet->id}}" class="modal">
                     <!-- Modal content -->
                     <div class="modal-content">
                         <span class="close">&times;</span>
                         <div class="col-md-8">
-                            <img class="img-fluid" id="alt_{{$pet->id}}" src="{{asset($pet->picture_src)}}" alt="">
+                            <img class="img-fluid-modal" id="alt_{{$pet->id}}" src="{{asset($pet->picture_src)}}" alt="">
                         </div>
 
                         <div class="col-md-4">
@@ -89,62 +148,7 @@
                     </div>
                     @endforeach
                 @endif
-                <!-- <div class="col-lg-3 col-md-4 col-xs-6">
-
-                    <img id="myImg1" class="img-fluid img-thumbnail" src="{{asset('image/pet/cat/1.jpg')}}" alt="">
-                </div>
-                <div class="col-lg-3 col-md-4 col-xs-6">
-                    <img id="myImg2" class="img-fluid img-thumbnail" src="{{asset('image/pet/cat/2.jpg')}}" alt="">
-                </div> -->
-
-                <!-- <div id="myModal1" class="modal"> -->
-
-                    <!-- Modal content -->
-                    <!-- <div class="modal-content">
-                        <span class="close">&times;</span>
-                        <div class="col-md-8">
-                            <img class="img-fluid" id="alt_1" src="{{asset('image/pet/cat/1.jpg')}}" alt="">
-                        </div>
-
-                        <div class="col-md-4">
-                            <h3 class="my-3">Project Description</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-                             <h3 class="my-3">Project Details</h3> -->
-                            <!--  <ul>
-                               <li>Lorem Ipsum</li>
-                               <li>Dolor Sit Amet</li>
-                               <li>Consectetur</li>
-                               <li>Adipiscing Elit</li>
-                             </ul> -->
-                        <!-- </div>
-                    </div>
-
-                </div>  -->
-
-                <!-- The Modal -->
-                <!-- <div id="myModal2" class="modal"> -->
-
-                    <!-- Modal content -->
-                    <!-- <div class="modal-content">
-                        <span class="close">&times;</span>
-                        <div class="col-md-8">
-                            <img class="img-fluid" id="alt_1" src={{asset('image/pet/cat/2.jpg')}} alt="">
-                        </div>
-
-                        <div class="col-md-4">
-                            <h3 class="my-3">Project Description</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-                            <h3 class="my-3">Project Details</h3>
-                            <ul>
-                                <li>Lorem Ipsum</li>
-                                <li>Dolor Sit Amet</li>
-                                <li>Consectetur</li>
-                                <li>Adipiscing Elit</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                </div> -->
+                
             </div>
         </div>
     </div>
