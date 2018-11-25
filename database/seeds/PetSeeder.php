98bb1b0c923fecd8085d8b64b3093f0fe0eb9f1c
Jordy Andreas <jordyandreas76@gmail.com>
2018-11-25 13:56:56 +0700

    nambah petSeeder(untuk dummy findlove)

--- database/seeds/PetSeeder.php
+++ database/seeds/PetSeeder.php
@@ -12,42 +12,265 @@ class PetSeeder extends Seeder
      */
     public function run()
     {
-        //
-        DB::table('pets')->insert([
-            'category_id' => 4,
-            'pet_name' => 'rabity',
-            'userOwner' => 10,
-            'age' => 5, //in month
+        // //
+        // DB::table('pets')->insert([
+        //     'category_id' => 4,
+        //     'pet_name' => 'rabity',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
             
-            'gender' => 'Female',
-            'status' => 0, //0 berarti lom di adopt ama manusia
-            'picture_src' => "image/pet/rabbit/2.jpg",
-            'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
-            'audit_activity' => 'I',
-        ]);
+        //     'gender' => 'Female',
+        //     'status' => 0, //0 berarti lom di adopt ama manusia
+        //     'picture_src' => "image/pet/rabbit/2.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
 
+        // DB::table('pets')->insert([
+        //     'category_id' => 4,
+        //     'pet_name' => 'rabito',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/rabbit/1.JPG",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 3,
+        //     'pet_name' => 'hamsty',
+        //     'userOwner' => 10,
+        //     'age' => 3, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/hamster/1.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        //anjing        
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Doggo',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/1.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Akaer',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/2.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Akaeo',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/1.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Akaeeo',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/2.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Akerao',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/1.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Akererao',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/2.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Eraw',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/1.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Popo',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/2.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Pipi',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/1.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+
+        // DB::table('pets')->insert([
+        //     'category_id' => 1,
+        //     'pet_name' => 'Akrrao',
+        //     'userOwner' => 10,
+        //     'age' => 5, //in month
+        //     'gender' => 'Male',
+        //     'status' => 0,
+        //     'picture_src' => "image/pet/dog/2.jpg",
+        //     'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+        //     'audit_activity' => 'I',
+        // ]);
+
+        //kucing
         DB::table('pets')->insert([
-            'category_id' => 4,
-            'pet_name' => 'rabito',
+            'category_id' => 1,
+            'pet_name' => 'Caety',
             'userOwner' => 10,
             'age' => 5, //in month
             'gender' => 'Male',
             'status' => 0,
-            'picture_src' => "image/pet/rabbit/1.JPG",
+            'picture_src' => "image/pet/cat/1.jpg",
             'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
             'audit_activity' => 'I',
         ]);
 
         DB::table('pets')->insert([
-            'category_id' => 3,
-            'pet_name' => 'hamsty',
+            'category_id' => 1,
+            'pet_name' => 'Cefde',
             'userOwner' => 10,
-            'age' => 3, //in month
+            'age' => 5, //in month
             'gender' => 'Male',
             'status' => 0,
-            'picture_src' => "image/pet/hamster/1.jpg",
+            'picture_src' => "image/pet/cat/2.jpg",
             'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
             'audit_activity' => 'I',
         ]);
+
+        DB::table('pets')->insert([
+            'category_id' => 1,
+            'pet_name' => 'Cearet',
+            'userOwner' => 10,
+            'age' => 5, //in month
+            'gender' => 'Male',
+            'status' => 0,
+            'picture_src' => "image/pet/cat/1.jpg",
+            'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+            'audit_activity' => 'I',
+        ]);
+
+        DB::table('pets')->insert([
+            'category_id' => 1,
+            'pet_name' => 'Cadad',
+            'userOwner' => 10,
+            'age' => 5, //in month
+            'gender' => 'Male',
+            'status' => 0,
+            'picture_src' => "image/pet/cat/2.jpg",
+            'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+            'audit_activity' => 'I',
+        ]);
+
+        DB::table('pets')->insert([
+            'category_id' => 1,
+            'pet_name' => 'Ceerte',
+            'userOwner' => 10,
+            'age' => 5, //in month
+            'gender' => 'Male',
+            'status' => 0,
+            'picture_src' => "image/pet/cat/1.jpg",
+            'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+            'audit_activity' => 'I',
+        ]);
+
+        DB::table('pets')->insert([
+            'category_id' => 1,
+            'pet_name' => 'Caadfe',
+            'userOwner' => 10,
+            'age' => 5, //in month
+            'gender' => 'Male',
+            'status' => 0,
+            'picture_src' => "image/pet/cat/2.jpg",
+            'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+            'audit_activity' => 'I',
+        ]);
+
+        DB::table('pets')->insert([
+            'category_id' => 1,
+            'pet_name' => 'Ceaea',
+            'userOwner' => 10,
+            'age' => 5, //in month
+            'gender' => 'Male',
+            'status' => 0,
+            'picture_src' => "image/pet/cat/1.jpg",
+            'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+            'audit_activity' => 'I',
+        ]);
+
+        DB::table('pets')->insert([
+            'category_id' => 1,
+            'pet_name' => 'Caeter',
+            'userOwner' => 10,
+            'age' => 5, //in month
+            'gender' => 'Male',
+            'status' => 0,
+            'picture_src' => "image/pet/cat/2.jpg",
+            'description' => "blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam blam",
+            'audit_activity' => 'I',
+        ]);
+
+
+        
     }
 }
